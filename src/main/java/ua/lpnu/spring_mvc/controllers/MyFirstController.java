bdaf9ba39a540965c62dc020bbe0404ea9155ccf
defr0stt <dima.sb713@gmail.com>
2022-02-04 15:34:08 +0200

    Added calculator page and opportunity to provide parameters in requests

--- src/main/java/ua/lpnu/spring_mvc/controllers/MyFirstController.java
+++ src/main/java/ua/lpnu/spring_mvc/controllers/MyFirstController.java
@@ -1,20 +1,47 @@
 package ua.lpnu.spring_mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class MyFirstController
 {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            Model model) {
+        model.addAttribute("message","Hello, " + name);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              Model model){
+        model.addAttribute("message","Goodbye, " + name);
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam(value = "action", required = false) String action,
+                            @RequestParam(value = "var1", required = false) Integer x1,
+                            @RequestParam(value = "var2", required = false) Integer x2,
+                            Model model){
+        String result = "NO PARAMETERS";
+        if(action != null && x1 != null && x2 != null){
+            if(action.equals("add")){
+                result = "Addition result = " + x1 + " + " + x2 + " = " + (x1 + x2);
+            } else if(action.equals("sub")){
+                result = "Subtraction result = " + x1 + " - " + x2 + " = " + (x1 - x2);
+            } else if(action.equals("div")){
+                result = "Division result = " + x1 + " / " + x2 + " = " + String.format("%.2f",(double)x1 / x2);
+            } else if(action.equals("mul")){
+                result = "Multiplication result : " + x1 + " * " + x2 + " = " + (x1 * x2);
+            }
+        }
+        model.addAttribute("result",result);
+        return "first/calculator";
     }
 }
